783661826de7c57c9f77fababcc9d2401d6678fb
RedBrumbler <redbrumbler.official+github@gmail.com>
2022-08-08 21:15:20 +0200

    Reduce example sizes to make them fit better

--- src/assets/BSMLExamples.tsx
+++ src/assets/BSMLExamples.tsx
@@ -40,7 +40,8 @@ export const ClickableImage = {
     exampleString: 
 `<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize'>
     <text text='Click to reload textures:' align='Center'/>
-    <stack pref-width='10' pref-height='10' vertical-fit='PreferredSize' horizontal-fit='PreferredSize'>
+    <stack pref-width='10' pref-height='10' 
+        vertical-fit='PreferredSize' horizontal-fit='PreferredSize'>
         <clickable-image src='#ReloadIcon' on-click='ReloadTextures'/>
     </stack>
 </vertical>`,
@@ -57,7 +58,8 @@ export const ClickableText = {
 
 export const ColorSetting = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
     <color-setting label='Saber Color' on-change='SaberColorPicked' apply-on-change='true'/>
 </vertical>`,
     exampleImage: `ColorSetting.jpg`
@@ -66,7 +68,8 @@ export const ColorSetting = {
 export const CustomList = {
     exampleString: 
 `<bg size-delta-x="65" size-delta-y="65" anchor-pos-x="0" anchor-pos-y="10">
-    <vertical horizontal-fit='PreferredSize' vertical-fit='PreferredSize' anchor-pos-x='-3' size-delta-y='0'>
+    <vertical horizontal-fit='PreferredSize' 
+        vertical-fit='PreferredSize' anchor-pos-x='-3' size-delta-y='0'>
         <custom-list data="customListData" pref-height='55' pref-width='55' show-scrollbar="true">
             <stack vertical-fit='Unconstrained'>
                 <horizontal pref-height="8.5" vertical-fit='Unconstrained' bg="round-rect-panel">
@@ -82,7 +85,8 @@ export const CustomList = {
 
 export const DropdownListSetting = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
     <dropdown-list-setting label='TrailType' options='trailTypes' on-change='TrailTypeChosen'/>
 </vertical>`,
     exampleImage: `DropdownListSetting.jpg`
@@ -116,7 +120,8 @@ export const Horizontal = {
 
 export const IconSegments = {
     exampleString: 
-`<vertical pref-width='40' pref-height='10' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+`<vertical pref-width='40' pref-height='10' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
     <icon-segments contents='segmentedIconData' select-cell='SelectedIcon'/>
 </vertical>`,
     exampleImage: `IconSegments.jpg`
@@ -125,7 +130,8 @@ export const IconSegments = {
 export const Image = {
     exampleString: 
 `<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize'>
-    <stack pref-width='10' pref-height='10' vertical-fit='PreferredSize' horizontal-fit='PreferredSize'>
+    <stack pref-width='10' pref-height='10' vertical-fit='PreferredSize' 
+        horizontal-fit='PreferredSize'>
         <image src='#ReloadIcon' on-click='ReloadTextures'/>
     </stack>
 </vertical>`,
@@ -134,15 +140,18 @@ export const Image = {
 
 export const IncrementSetting = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
-    <increment-setting label="Note Size" min='0.05' max='2' increment='0.05' digits='2' on-change='SetSize'/>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
+    <increment-setting label="Note Size" min='0.05' max='2' 
+        increment='0.05' digits='2' on-change='SetSize'/>
 </vertical>`,
     exampleImage: `IncrementSetting.jpg`
 }
 
 export const LeaderBoard = {
     exampleString: 
-`<vertical  bg='round-rect-panel' pref-width='90' pref-height='65' vertical-fit='PreferredSize' pad='2'>
+`<vertical  bg='round-rect-panel' pref-width='90' pref-height='65' 
+    vertical-fit='PreferredSize' pad='2'>
     <leaderboard id='lb'/>
 </vertical>`,
     exampleImage: `LeaderBoard.jpg`
@@ -151,7 +160,8 @@ export const LeaderBoard = {
 export const List = {
     exampleString: 
 `<bg size-delta-x="65" size-delta-y="65" anchor-pos-x="0" anchor-pos-y="10">
-    <vertical bg='round-rect-panel' horizontal-fit='PreferredSize' vertical-fit='PreferredSize' anchor-pos-x='-3' size-delta-y='0' pad='2'>
+    <vertical bg='round-rect-panel' horizontal-fit='PreferredSize' vertical-fit='PreferredSize' 
+        anchor-pos-x='-3' size-delta-y='0' pad='2'>
         <list content='listData' on-select='ItemSelected' show-scrollbar='true'/>
     </vertical>
 </bg>`,
@@ -160,7 +170,8 @@ export const List = {
 
 export const ListSetting = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
     <list-setting label='Text' choices='lyrics' on-select='SelectedChoice'/>
 </vertical>`,
     exampleImage: `ListSetting.jpg`
@@ -168,7 +179,8 @@ export const ListSetting = {
 
 export const ListSliderSetting = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
     <list-slider-setting label='Text' choices='lyrics' on-select='SelectedChoice'/>
 </vertical>`,
     exampleImage: `ListSliderSetting.jpg`
@@ -176,7 +188,8 @@ export const ListSliderSetting = {
 
 export const Loading = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
     <stack pref-width='20' pref-height='20'>
         <loading pref-width='20' pref-height='20' preserve-aspect='true'/>
     </stack>
@@ -186,10 +199,13 @@ export const Loading = {
 
 export const Modal = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
     <button label='Show Modal' on-click='myModal#Show'/>
-    <modal id='myModal' pref-height='10' pref-width='30' click-off-closes='true' move-to-center='false' anchor-pos-x='-40' anchor-pos-y='20'>
-        <vertical pref-width='40' pref-height='20' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+    <modal id='myModal' pref-height='10' pref-width='30' click-off-closes='true' 
+        move-to-center='false' anchor-pos-x='-40' anchor-pos-y='20'>
+        <vertical pref-width='40' pref-height='20' bg='round-rect-panel' 
+            vertical-fit='PreferredSize' pad='2'>
             <text text='Hello' align='Center'/>
         </vertical>
     </modal>
@@ -199,7 +215,8 @@ export const Modal = {
 
 export const ModalColorPicker = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
     <button on-click='PickAColor'/>
     <modal-color-picker id='colorPicker'/>
 </vertical>`,
@@ -217,7 +234,8 @@ export const ModalKeyboard = {
 
 export const Modifier = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
     <modifier label='Custom Modifier' on-chang='ModifierToggled'/>
 </vertical>`,
     exampleImage: `Modifier.jpg`
@@ -225,8 +243,10 @@ export const Modifier = {
 
 export const ModifierContainer = {
     exampleString: 
-`<modifier-container label='Custom Modifier' on-chang='ModifierToggled' pref-width='80' pref-height='50' size-delta-x='89'>
-    <grid bg='round-rect-panel' cell-size-x='25' spacing-x='2' cell-size-y='10' spacing-y='2' pref-width='80' pref-height='50' pad='2'>
+`<modifier-container label='Custom Modifier' on-chang='ModifierToggled' 
+    ref-width='80' pref-height='50' size-delta-x='89'>
+    <grid bg='round-rect-panel' cell-size-x='25' spacing-x='2' cell-size-y='10' 
+        spacing-y='2' pref-width='80' pref-height='50' pad='2'>
         <macro.repeat count='12'>
             <modifier label='Custom Modifier'/>
         </macro.repeat>
@@ -238,18 +258,23 @@ export const ModifierContainer = {
 export const PageButton = {
     exampleString: 
 `<bg>
-    <vertical anchor-pos-x="60" pref-height="80" pref-width="10" vertical-fit="PreferredSize">
+    <vertical anchor-pos-x="60" pref-height="80" pref-width="10" 
+        vertical-fit="PreferredSize">
         <horizontal>
-            <page-button text="Up" tags="PageUpFor:cont" direction="Up" pref-height="7.5" pref-width="7.5"/>
+            <page-button text="Up" tags="PageUpFor:cont" direction="Up" 
+                pref-height="7.5" pref-width="7.5"/>
         </horizontal>
-        <horizontal vertical-fit="Unconstrained" horizontal-fit="MinSize" min-width="1.5" pref-width="1.5" pref-height="65">
+        <horizontal vertical-fit="Unconstrained" horizontal-fit="MinSize" 
+            min-width="1.5" pref-width="1.5" pref-height="65">
             <scroll-indicator tags="IndicatorFor:cont"/>
         </horizontal>
         <horizontal>
-            <page-button tags="PageDownFor:cont" direction="Down" pref-height="7.5" pref-width="7.5"/>
+            <page-button tags="PageDownFor:cont" direction="Down" 
+                pref-height="7.5" pref-width="7.5"/>
         </horizontal>
     </vertical>
-    <scrollable-container size-delta-x="-54" horizontal-fit="PreferredSize" id="cont">
+    <scrollable-container size-delta-x="-54" horizontal-fit="PreferredSize" 
+        id="cont">
         <macro.repeat count='10'>
             <button label='A Button'/>
         </macro.repeat>
@@ -260,8 +285,10 @@ export const PageButton = {
 
 export const RawImage = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
-    <stack pref-width='10' pref-height='10' horizontal-fit='PreferredSize' vertical-fit='PreferredSize'>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
+    <stack pref-width='10' pref-height='10' horizontal-fit='PreferredSize' 
+        vertical-fit='PreferredSize'>
         <raw-image src='#BillieEilishCover'/>
     </stack>
 </vertical>`,
@@ -271,18 +298,23 @@ export const RawImage = {
 export const ScrollIndicator = {
     exampleString: 
 `<bg>
-    <vertical anchor-pos-x="60" pref-height="80" pref-width="10" vertical-fit="PreferredSize">
+    <vertical anchor-pos-x="60" pref-height="80" pref-width="10" 
+        vertical-fit="PreferredSize">
         <horizontal>
-            <page-button text="Up" tags="PageUpFor:cont" direction="Up" pref-height="7.5" pref-width="7.5"/>
+            <page-button text="Up" tags="PageUpFor:cont" direction="Up" 
+                pref-height="7.5" pref-width="7.5"/>
         </horizontal>
-        <horizontal vertical-fit="Unconstrained" horizontal-fit="MinSize" min-width="1.5" pref-width="1.5" pref-height="65">
+        <horizontal vertical-fit="Unconstrained" horizontal-fit="MinSize" 
+            min-width="1.5" pref-width="1.5" pref-height="65">
             <scroll-indicator tags="IndicatorFor:cont"/>
         </horizontal>
         <horizontal>
-            <page-button tags="PageDownFor:cont" direction="Down" pref-height="7.5" pref-width="7.5"/>
+            <page-button tags="PageDownFor:cont" direction="Down" 
+                pref-height="7.5" pref-width="7.5"/>
         </horizontal>
     </vertical>
-    <scrollable-container size-delta-x="-54" horizontal-fit="PreferredSize" id="cont">
+    <scrollable-container size-delta-x="-54" horizontal-fit="PreferredSize" 
+        id="cont">
         <macro.repeat count='10'>
             <button label='A Button'/>
         </macro.repeat>
@@ -306,18 +338,23 @@ export const ScrollView = {
 export const ScrollableContainer = {
     exampleString: 
 `<bg>
-    <vertical anchor-pos-x="60" pref-height="80" pref-width="10" vertical-fit="PreferredSize">
+    <vertical anchor-pos-x="60" pref-height="80" pref-width="10" 
+        vertical-fit="PreferredSize">
         <horizontal>
-            <page-button text="Up" tags="PageUpFor:cont" direction="Up" pref-height="7.5" pref-width="7.5"/>
+            <page-button text="Up" tags="PageUpFor:cont" direction="Up" 
+                pref-height="7.5" pref-width="7.5"/>
         </horizontal>
-        <horizontal vertical-fit="Unconstrained" horizontal-fit="MinSize" min-width="1.5" pref-width="1.5" pref-height="65">
+        <horizontal vertical-fit="Unconstrained" horizontal-fit="MinSize" 
+            min-width="1.5" pref-width="1.5" pref-height="65">
             <scroll-indicator tags="IndicatorFor:cont"/>
         </horizontal>
         <horizontal>
-            <page-button tags="PageDownFor:cont" direction="Down" pref-height="7.5" pref-width="7.5"/>
+            <page-button tags="PageDownFor:cont" direction="Down" 
+                pref-height="7.5" pref-width="7.5"/>
         </horizontal>
     </vertical>
-    <scrollable-container size-delta-x="-54" horizontal-fit="PreferredSize" id="cont">
+    <scrollable-container size-delta-x="-54" horizontal-fit="PreferredSize" 
+        id="cont">
         <macro.repeat count='10'>
             <button label='A Button'/>
         </macro.repeat>
@@ -340,17 +377,21 @@ export const SettingsScrollView = {
 
 export const SliderSetting = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
-    <slider-setting label="Note Size" min='0.05' max='2' increment='0.05' digits='2' on-change='SetSize'/>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
+    <slider-setting label="Note Size" min='0.05' max='2' increment='0.05' 
+        digits='2' on-change='SetSize'/>
 </vertical>`,
     exampleImage: `SliderSetting.jpg`
 }
 
 export const Stack = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
     <stack pref-width='60' pref-height='30'>
-        <image src='#ClockIcon' img-color='#ff0000' preserve-aspect='true' skew='0.18'/>
+        <image src='#ClockIcon' img-color='#ff0000' 
+            preserve-aspect='true' skew='0.18'/>
         <text text='It is Time' align='Center'/>
     </stack>
 </vertical>`,
@@ -359,7 +400,8 @@ export const Stack = {
 
 export const StringSetting = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
     <string-setting label="User name" on-change='SetUserName'/>
 </vertical>`,
     exampleImage: `StringSetting.jpg`
@@ -368,7 +410,8 @@ export const StringSetting = {
 export const Tab = {
     exampleString: 
 `<bg>
-    <tab-selector tab-tag="main" anchor-pos-y='0' size-delta-y='6' size-delta-x='-60' child-expand-width='true' child-control-width='true'/>
+    <tab-selector tab-tag="main" anchor-pos-y='0' size-delta-y='6' size-delta-x='-60' 
+        child-expand-width='true' child-control-width='true'/>
     <tab tags="main" tab-name="first">
         <text text='First Tab' align='Center'/>
     </tab>
@@ -385,7 +428,8 @@ export const Tab = {
 export const TabSelect = {
     exampleString: 
 `<bg>
-    <tab-selector tab-tag="main" anchor-pos-y='0' size-delta-y='6' size-delta-x='-60' child-expand-width='true' child-control-width='true'/>
+    <tab-selector tab-tag="main" anchor-pos-y='0' size-delta-y='6' size-delta-x='-60' 
+        child-expand-width='true' child-control-width='true'/>
     <tab tags="main" tab-name="first">
         <text text='First Tab' align='Center'/>
     </tab>
@@ -401,9 +445,11 @@ export const TabSelect = {
 
 export const Text = {
     exampleString: 
-`<vertical pref-width='60' pref-height='50' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+`<vertical pref-width='60' pref-height='50' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
     <text text="Holy shit it's" align='Center' font-size='8'/>
-    <img src='#CamelliaCover' preserve-aspect='true' pref-width='40' pref-height='40'/>
+    <img src='#CamelliaCover' preserve-aspect='true' 
+        pref-width='40' pref-height='40'/>
     <text text="it's camellia" align='Center' font-size='8'/>
 </vertical>`,
     exampleImage: `Text.jpg`
@@ -421,7 +467,8 @@ export const TextPage = {
 
 export const TextSegments = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
     <text-segments contents='TextSegmentedItemList' select-cell='SelectedCell'/>
 </vertical>`,
     exampleImage: `TextSegments.jpg`
@@ -429,7 +476,8 @@ export const TextSegments = {
 
 export const ToggleSetting = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
     <toggle-setting label="Enable Mod" on-change='SetEnabled'/>
 </vertical>`,
     exampleImage: `ToggleSetting.jpg`
@@ -447,7 +495,8 @@ export const Vertical = {
 
 export const VerticalIconSegments = {
     exampleString: 
-`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' vertical-fit='PreferredSize' pad='2'>
+`<vertical pref-width='60' pref-height='30' bg='round-rect-panel' 
+    vertical-fit='PreferredSize' pad='2'>
     <vertical-icon-segments contents='SegmentedIconData' select-cell='SelectedIcon'/>
 </vertical>`,
     exampleImage: `VerticalIconSegments.jpg`
